72d27604d75a44b648ca1628ead5bb64ddad5011
payalrana-11 <131985698+payalrana-11@users.noreply.github.com>
2026-07-04 12:05:07 +0530

    Update BASE_URL to production endpoint

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://ai-learning-assistant-backend-0k79.onrender.com";
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
